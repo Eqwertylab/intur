585c72d133e98752f5c99948b138cc1a9aef1392
Eqwertylab <79225555735@ya.ru>
2015-06-11 00:39:48 +0500

    add hot slider

--- index.php
+++ index.php
@@ -1,58 +1,137 @@
 <!DOCTYPE html>
 <html class="">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Интур - Главная</title>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="css/style.css">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Интур - Главная</title>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<aside class="sidebar_mob">
-		<ul class="menu_mob">
-			<li><a href="">Главная</a></li>
-			<li><a href="">Подобрать тур</a></li>
-			<li><a href="">О нас</a></li>
-			<li><a href="">Статьи</a></li>
-			<li><a href="">Контакты</a></li>
-		</ul>
-	</aside>
-	<div class="wrapper">
-		<div class="layout">
-			<header class="header_mob visible-xs">
-				
-				<div class="page_title">Заголовок</div>
+  <aside class="sidebar_mob">
+    <ul class="menu_mob">
+      <li><a href="">Главная</a></li>
+      <li><a href="">Подобрать тур</a></li>
+      <li><a href="">О нас</a></li>
+      <li><a href="">Статьи</a></li>
+      <li><a href="">Контакты</a></li>
+    </ul>
+  </aside>
+  <div class="wrapper">
+    <div class="layout">
 
-				<div class="nav_mob_toggle" id="nav_mob_toggle">
-					<span></span>
-					<span></span>
-					<span></span>
-				</div>
-				
-				<a href="" class="logo_mob"></a>	
-			</header>
-			<header class="header">
+      <header class="header_mob visible-xs">
+        
+        <div class="page_title">Заголовок</div>
 
-				<a href="#" class="logo"></a>
+        <div class="nav_mob_toggle" id="nav_mob_toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        
+        <a href="" class="logo_mob"></a>  
+      </header>
 
-				<ul class="menu">
-					<li><a href="">Главная</a></li>
-					<li><a href="">Подобрать тур</a></li>
-					<li><a href="">О нас</a></li>
-					<li><a href="">Статьи</a></li>
-					<li><a href="">Контакты</a></li>
-				</ul>
-			</header>
+      <header class="header hidden-xs">
 
-			<main class="main">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non ab, placeat iste temporibus soluta, mollitia quisquam. Esse praesentium, libero veritatis, tempore dignissimos pariatur voluptas minima, voluptatem facere perspiciatis laborum suscipit.
-			</main>
-		</div>
-		<footer class="footer"></footer>
-	</div>
-	<div class="curtain" id="curtain"></div>
-	<script src="https://yastatic.net/jquery/2.1.3/jquery.min.js"></script>
-	<script src="js/app.js"></script>
+        <a href="#" class="logo"></a>
+
+        <ul class="menu">
+          <li><a href="">Главная</a></li>
+          <li><a href="">Подобрать тур</a></li>
+          <li><a href="">О нас</a></li>
+          <li><a href="">Статьи</a></li>
+          <li><a href="">Контакты</a></li>
+        </ul>
+      </header>
+
+      <main class="main">
+
+        <section class="slider_hot">
+
+          <div class="container">
+
+            <select name="" id="" class="select_country">
+              <option value="Испания">Испания</option>
+              <option value="Франция">Франция</option>
+              <option value="Чехия">Чехия</option>
+            </select>
+            
+            <div class="shgroup_wrapp">
+              
+              <div class="shgroup" data-country="Испания">
+
+                <div class="shgroup_item">
+                  <div class="shgroup_city">Барселона</div>
+                  <div class="shgroup_date">19.06 - 30.06</div>
+                  <div class="shgroup_price">23 000 р.</div>
+                </div>
+
+                <div class="shgroup_item">
+                  <div class="shgroup_city">Мадрид</div>
+                  <div class="shgroup_date">20.07 - 05.08</div>
+                  <div class="shgroup_price">26 000 р.</div>
+                </div>
+
+                <div class="shgroup_item">
+                  <div class="shgroup_city">Севилья</div>
+                  <div class="shgroup_date">30.09 - 15.10</div>
+                  <div class="shgroup_price">24 000 р.</div>
+                </div>
+              </div>
+
+              <div class="shgroup" data-country="Франция">
+
+                <div class="shgroup_item">
+                  <div class="shgroup_city">Париж</div>
+                  <div class="shgroup_date">19.06 - 30.06</div>
+                  <div class="shgroup_price">23 000 р.</div>
+                </div>
+
+                <div class="shgroup_item">
+                  <div class="shgroup_city">Марсель</div>
+                  <div class="shgroup_date">20.07 - 05.08</div>
+                  <div class="shgroup_price">26 000 р.</div>
+                </div>
+
+                <div class="shgroup_item">
+                  <div class="shgroup_city">Лион</div>
+                  <div class="shgroup_date">30.09 - 15.10</div>
+                  <div class="shgroup_price">24 000 р.</div>
+                </div>
+              </div>
+
+              <div class="shgroup" data-country="Чехия">
+
+                <div class="shgroup_item">
+                  <div class="shgroup_city">Прага</div>
+                  <div class="shgroup_date">19.06 - 30.06</div>
+                  <div class="shgroup_price">23 000 р.</div>
+                </div>
+
+                <div class="shgroup_item">
+                  <div class="shgroup_city">Брно</div>
+                  <div class="shgroup_date">20.07 - 05.08</div>
+                  <div class="shgroup_price">26 000 р.</div>
+                </div>
+
+                <div class="shgroup_item">
+                  <div class="shgroup_city">Карловы Вары</div>
+                  <div class="shgroup_date">30.09 - 15.10</div>
+                  <div class="shgroup_price">24 000 р.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+    <footer class="footer"></footer>
+  </div>
+  <div class="curtain" id="curtain"></div>
+  <script src="https://yastatic.net/jquery/2.1.3/jquery.min.js"></script>
+  <script src="js/app.js"></script>
 </body>
 </html>
